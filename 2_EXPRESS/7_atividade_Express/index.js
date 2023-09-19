9dba1446ff70f404cb6c56ab15578c587c4180d4
Leonardo Sodré <leonardo.f.sodre@gmail.com>
2023-09-19 16:36:20 -0300

    98% da atividade

--- 2_EXPRESS/7_atividade_Express/index.js
+++ 2_EXPRESS/7_atividade_Express/index.js
@@ -1,4 +1,3 @@
-const exp = require('constants')
 const express = require('express')
 const app = express()
 
@@ -13,11 +12,19 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-app.post('/index', (req, res) =>{
-    
+app.post('/index/enviar', (req, res) =>{
+    const nome = req.body.nome
+    const email = req.body.email
+
+    console.log(`
+    Usuário: ${nome} 
+    Email: ${email}
+    `)
+
+    res.redirect('/')
 })
 
-app.get('/', (res, res) =>{
+app.get('/', (req, res) =>{
     res.sendFile(`${basePath}/index.html `)
 })
 
