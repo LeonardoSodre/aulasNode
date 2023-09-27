e06ecfb0cb86704651af4ca127d47f132411dd61
Leonardo Sodré <leonardo.f.sodre@gmail.com>
2023-09-26 22:33:14 -0300

    Handlebars

---
+++ 3_HANDLEBARS/2_layouts/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+//Configurando o HandleBards com Template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/',(req,res) =>{ //rota padrão para o usuario que acessar a pagina
+    res.render('home', {layout: false}) 
+})
+
+app.listen(3030, () => {
+    console.log('Servidor rodando na porta 3030')
+})
